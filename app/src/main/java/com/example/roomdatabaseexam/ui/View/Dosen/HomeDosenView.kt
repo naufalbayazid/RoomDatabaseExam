30ee61f15807324015dd3ad1bf52476756d09f54
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-23 18:02:38 +0700

    membuat tampilan untuk home dosen view

--- app/src/main/java/com/example/roomdatabaseexam/ui/View/Dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/roomdatabaseexam/ui/View/Dosen/HomeDosenView.kt
@@ -1,2 +1,233 @@
 package com.example.roomdatabaseexam.ui.View.Dosen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.roomdatabaseexam.ui.customWidget.TopAppBar
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabaseexam.Entity.Dosen
+import com.example.roomdatabaseexam.ui.VMDosen.HomeDosenviewModel
+import com.example.roomdatabaseexam.ui.VMDosen.HomeUiState
+import com.example.roomdatabaseexam.ui.VMDosen.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun HomeDosenView(
+    viewModel: HomeDosenviewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDsn: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    onBack: () -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier,
+
+                )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen"
+
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUIState.collectAsState()
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFF0F0F0)) // Warna abu muda
+        ) {
+            BodyHomeDsnView(
+                homeUiState = homeUiState,
+                onClick = { onDetailClick(it) },
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun BodyHomeDsnView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isloading -> {
+            //menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            //Menampilkan pesan Error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch{
+                        snackbarHostState.showSnackbar(message) //tampilkan Snackbar
+                    }
+                }
+            }
+        }
+        homeUiState.listDsn.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data Dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            // Menampilkan daftar Dosen
+            ListDosen(
+                listDsn = homeUiState.listDsn,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+@Composable
+fun ListDosen(
+    listDsn: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listDsn,
+            itemContent = { dosen ->
+                CardDsn(
+                    dosen = dosen,
+                    onClick = { onClick(dosen.nidn) }
+                )
+            }
+        )
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDsn(
+    dosen: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = androidx.compose.material3.CardDefaults.cardColors(
+            containerColor = Color(0xFF001F54) // Warna navy
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White // Warna teks putih
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Create, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.nidn,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.White // Warna teks putih
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Face, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.JenisKelamin,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White // Warna teks putih
+                )
+            }
+        }
+    }
+}
